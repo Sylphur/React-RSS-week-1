32ee4b03763177072f12f00503e123cb2a5ac5ad
Evgeny Redchenko <neosylphur@gmail.com>
2023-11-01 18:56:21 +0400

    refactor: remove unnecessary search code

--- rss-react-1/src/services/api.service.tsx
+++ rss-react-1/src/services/api.service.tsx
@@ -27,32 +27,3 @@ export const searchPokemon = (name: string) => {
 export const searchPokemonList = () => {
   return fetch(`https://pokeapi.co/api/v2/pokemon/`);
 };
-
-export const doSearch = async (param: string): Promise<PokemonResponse[]> => {
-  const result: PokemonResponse[] = [];
-
-  if (param === '') {
-    searchPokemonList()
-      .then((res) => res.json())
-      .then((list: PokemonListResponse) => {
-        console.log(list);
-        const requests = list.results
-          .slice(0, 9)
-          .map((item: PokemonUrl) => searchPokemon(item.name));
-        Promise.all(requests).then((response) => {
-          const responses = response.map((res) => res.json());
-          Promise.all(responses).then((pokemons: PokemonResponse[]) => {
-            pokemons.forEach((pokemon) => result.push(pokemon));
-            return result;
-          });
-        });
-      });
-  } else {
-    searchPokemon(param)
-      .then((response) => response.json())
-      .then((res: PokemonResponse) => {
-        return result.push(res);
-      });
-  }
-  return result;
-};
