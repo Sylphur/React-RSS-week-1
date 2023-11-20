f2a9606b53d765be1296911535f0c6d49031bd3e
Evgeny Redchenko <neosylphur@gmail.com>
2023-11-20 04:01:11 +0400

    feat: connect reducers, middleware

--- rss-react-1/src/state/store.ts
+++ rss-react-1/src/state/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { pokemonAPI } from "../services/api-query.service";
+import searchSlice from "./reducers/searchSlice";
+import pokemonSlice from "./reducers/pokemonSlice";
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [pokemonAPI.reducerPath]: pokemonAPI.reducer,
+    search: searchSlice,
+    pokemon: pokemonSlice
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(pokemonAPI.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
