9b82c2b99f1ee19c5448443763404fb84655ec8b
Evgeny Redchenko <neosylphur@gmail.com>
2023-11-26 17:57:02 +0400

    fix: remove old vite files

--- rss-react-1/src/services/api.service.tsx
+++
@@ -1,9 +0,0 @@
-export const searchPokemon = (name: string) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-};
-
-export const searchPokemonList = (limit: number, offset: number) => {
-  return fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-  );
-};
--- rss-react-1/src/shared/constants.tsx
+++
@@ -1,1 +0,0 @@
-export const possiblePageSize = [4, 8, 12, 20];
