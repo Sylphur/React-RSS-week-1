9e1f59e61ddfd2e2117aedf954c2ce2373e75617
Evgeny Redchenko <neosylphur@gmail.com>
2023-11-26 17:54:36 +0400

    feat: setup next, eslint

---
+++ rss-react-1/rss-react/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function NotFoundPage() {
+  const router = useRouter();
+  console.log(router);
+  
+  return (
+    <>
+      <p>Page is not found!</p>
+    </>
+  )
+}
