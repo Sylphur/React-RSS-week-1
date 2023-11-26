c42d10c8f7fff33f0e2774f521c849bf299f1d79
Evgeny Redchenko <neosylphur@gmail.com>
2023-11-26 18:21:10 +0400

    feat: setup prettier, add husky

--- rss-react-1/rss-react/src/pages/404.tsx
+++ rss-react-1/rss-react/src/pages/404.tsx
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 export default function NotFoundPage() {
   const router = useRouter();
   console.log(router);
-  
+
   return (
     <>
       <p>Page is not found!</p>
     </>
-  )
+  );
 }
